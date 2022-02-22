5aac173f16ba7920abf47d16834e92f12a476662
smtandabuzo <sazi.mtandabuzo@metropolitan.co.za>
2022-02-22 16:57:19 +0200

    Project done and dusted

---
+++ script.js
@@ -0,0 +1,82 @@
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+// Calculate first and second value depending on operator
+const calculate = {
+    '/': (firstNumber,secondNumber) => firstNumber / secondNumber,
+
+    '*': (firstNumber,secondNumber) => firstNumber * secondNumber,
+
+    '+': (firstNumber,secondNumber) => firstNumber + secondNumber,
+
+    '-': (firstNumber,secondNumber) => firstNumber - secondNumber,
+
+    '=': (firstNumber,secondNumber) => secondNumber,
+}
+
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
+function sendNumberValue(number){
+    // Replace current value displayed if current value is entered
+    if(awaitingNextValue){
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false;
+    } else {
+    // If current display value is 0, replace it otherwise add it
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
+}
+
+function addDecimal(){
+    // If operator pressed, don't add decimal
+    if(awaitingNextValue) return;
+    // If no decimal,add one
+    if(!calculatorDisplay.textContent.includes('.')){
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
+
+function useOperator(operator){
+    const currentValue = Number(calculatorDisplay.textContent);
+    // Prevent multiple operators
+    if(operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    };
+    // Assign first value if first value doesn't exist
+    if(!firstValue){
+        firstValue = currentValue;
+    } else {
+        const calculation = calculate[operatorValue](firstValue,currentValue);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
+    }
+    awaitingNextValue = true;
+    operatorValue = operator;
+}
+
+// Reset all values,display
+function resetAll(){
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
+    calculatorDisplay.textContent = '0';
+}
+
+// Add event listeners for numbers, operators and decimal buttons
+inputBtns.forEach((inputBtn)=>{
+    if(inputBtn.classList.length === 0){
+        inputBtn.addEventListener('click',()=> sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')){
+        inputBtn.addEventListener('click',()=> useOperator(inputBtn.value));
+    } else if (inputBtn.classList.contains('decimal')){
+        inputBtn.addEventListener('click',()=> addDecimal());
+    }
+});
+
+// Event listener
+clearBtn.addEventListener('click',resetAll);
